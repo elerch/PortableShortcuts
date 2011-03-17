0982521bf2d56ec1b1063b14cbb59b9f54d3b8a9
Emil Lerch <emil@lerch.org>
2011-03-17 16:59:01 -0700

    Looking better - checkboxes removed from directories

---
+++ PortableShortcuts/Directory Resources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PortableShortcuts
+{
+    partial class Directory_Resources
+    {
+        public void AddEntry(object sender, EventArgs e)
+        {
+
+        }
+
+        public void RemoveEntry(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
